92aaeecd1852f2b8efaa6466c038d932bfe89fc3
kot6egemot <ex-2k@yandex.ru>
2023-05-05 18:46:46 +0300

    Персона: Создание/Обновление/Удаление(Без фото)

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,3 +1,5 @@
+import random
+
 import paho.mqtt.client as mqtt
 import json
 import time
@@ -6,10 +8,11 @@ client = mqtt.Client("my_client")
 client.username_pw_set('admin', 'admin123')
 client.connect("192.168.1.130", 8086, 60)
 client.loop_start()
-
+# USER_ID = random.randint(100, 10000)
+USER_ID = 71
 create_person_payload = {
     "type": 3,
-    "id": 7778,
+    "id": 222,
     "devSn": "YGKJ202107TR08EL0007",
     "feedbackUrl": "http://192.168.1.130:8080",
     "operations":
@@ -18,11 +21,11 @@ create_person_payload = {
                 "createBy": "",
                 "createTime": 1602843134000,
                 "deptId": 104,
-                "id": 10,
+                "id": USER_ID,
                 "sex": 0,
                 "status": 0,
                 "updateBy": "",
-                "userCode": "003",
+                "userCode": str(USER_ID),
                 "userName": "John",
                 "firstName": "Sergey",
                 "lastName": "Kuznetsov",
@@ -45,29 +48,26 @@ update_person_payload = {
     "devSn": "YGKJ202107TR08EL0007",
     "feedbackUrl": "",
     "operations":
-        [
-            {
-                "createBy": "",
-                "deptId": 104,
-                "id": 10,
-                "sex": 0,
-                "status": 0,
-                "updateBy": "",
-                "userCode": "003",
-                "userName": "John",
-                "firstName": "John",
-                "lastName": "John",
-                "userPhone": "15575681394",
-                "cardNum": "ABCD",
-                "wiegandNum": "100",
-                "company": "",
-                "department": "",
-                "group": "",
-                "remark": "",
-                "expiry": ""
-            }
-        ]
-
+        {
+            "createBy": "",
+            "deptId": 104,
+            "id": USER_ID,
+            "sex": 0,
+            "status": 0,
+            "updateBy": "",
+            "userCode": str(USER_ID),
+            "userName": "John",
+            "firstName": "John",
+            "lastName": "John",
+            "userPhone": "15575681394",
+            "cardNum": "ABCD",
+            "wiegandNum": str(USER_ID),
+            "company": "",
+            "department": "",
+            "group": "",
+            "remark": "",
+            "expiry": ""
+        }
 }
 
 query_person_payload = {
@@ -76,7 +76,7 @@ query_person_payload = {
     "devSn": "YGKJ202107TR08EL0007",
     "feedbackUrl": "",
     "operations": {
-        "emp_id": "003",
+        "emp_id": "",
         "keyword": "",
         "need_feature": False,
         "need_photo": False,
@@ -90,23 +90,19 @@ delete_person_payload = {
     "id": 1,
     "operations":
         [{
-            "id": 10,
+            "id": USER_ID,
             "params": {}
         }],
     "type": 5
 }
 
-# client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(create_person_payload))
-# time.sleep(1)
-client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(query_person_payload))
+client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(create_person_payload))
 time.sleep(1)
 client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(update_person_payload))
+time.sleep(2)
+client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(query_person_payload))
+client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(query_person_payload))
 time.sleep(1)
-
-# client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(query_person_payload))
-# time.sleep(1)
-#
-# client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(delete_person_payload))
-# time.sleep(1)
-# client.loop_stop()
-
+client.publish("/_dispatch/command/YGKJ202107TR08EL0007", json.dumps(delete_person_payload))
+time.sleep(1)
+client.loop_stop()
