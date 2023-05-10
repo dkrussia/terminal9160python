18121b0be1d55e440ac0c7bb6adc8056d35c5318
kot6egemot <ex-2k@yandex.ru>
2023-05-10 14:12:45 +0300

    Добавил маршрут для раздачи фотографий

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import os
 from pprint import pprint
 
 import uvicorn
 from fastapi import FastAPI, Request
+from starlette.staticfiles import StaticFiles
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+PHOTO_DIR = f"{BASE_DIR}/photo"
 
 app = FastAPI()
+app.mount("/photo", StaticFiles(directory=PHOTO_DIR), name="photo")
+
+print("BASE DIR: ", BASE_DIR)
+print("PHOTO DIR: ", PHOTO_DIR)
 
 
 # Test
-# При добавлении персон в терминал, надо дождатся результата вполнения
+# При добавлении персон в терминал, надо дождаться результата выполнения
 # и затем уже отправлять снова.
 # Если надо загрузить много персон сразу, то делаем это пачками по 100 человек.
 
@@ -41,7 +50,7 @@ async def device_login(request: Request):
             "token": "token",
         },
         "desc": "123",
-        "success": False
+        "success": True
     }
 
 
--- services/person_photo.py
+++ services/person_photo.py
@@ -1,9 +1,20 @@
 import base64
+import os
+
+from main import PHOTO_DIR
 
 
 class PersonPhoto:
+
     @staticmethod
     def base64_to_file(person_id: int, photo_base64: str):
         decoded_data = base64.b64decode(photo_base64)
-        with open(f"../photo/{person_id}.jpg", "wb") as fh:
+        with open(f"{PHOTO_DIR}/{person_id}.jpg", "wb") as fh:
             fh.write(decoded_data)
+
+    @staticmethod
+    def remove_photo(person_id: int, ):
+        try:
+            os.remove(f"{PHOTO_DIR}/{person_id}.jpg")
+        except FileNotFoundError:
+            pass
