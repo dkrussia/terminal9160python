dd650fee22a542138d275626364f518d0ce78cf8
kot6egemot <ex-2k@yandex.ru>
2023-07-17 23:19:23 +0300

    fix

--- base/log.py
+++ base/log.py
@@ -5,13 +5,13 @@ logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-file_handler = logging.FileHandler('../logs/logfile.log')
+file_handler = logging.FileHandler('./logs/logfile.log')
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 
-# logger.debug('Это отладочное сообщение')
-# logger.info('Это информационное сообщение')
-# logger.warning('Это предупреждающее сообщение')
-# logger.error('Это сообщение об ошибке')
+logger.debug('Это отладочное сообщение')
+logger.info('Это информационное сообщение')
+logger.warning('Это предупреждающее сообщение')
+logger.error('Это сообщение об ошибке')
