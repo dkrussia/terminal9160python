83cc00fcb6eea880ca8de831d968fa0834ecaa44
kot6egemot <ex-2k@yandex.ru>
2023-07-19 17:18:54 +0300

    add env files for saved settings of container

--- docker-release/runner/main_exe.py
+++ docker-release/runner/main_exe.py
@@ -1,10 +1,46 @@
+import json
+
 import docker
 import dearpygui.dearpygui as dpg
 from docker.errors import APIError
+from docker.models.containers import Container
 
 client = docker.from_env()
 image_search_string = '9160'
 container_search_string = '-9160-container'
+
+
+class DokerEnv:
+    env = {}
+
+    @staticmethod
+    def read_env():
+        try:
+            with open("env.json", "r") as f:
+                DokerEnv.env = json.load(f)
+                return DokerEnv.env
+        except FileNotFoundError:
+            DokerEnv.env = {
+                "HOST": "127.0.0.1",
+                "TZ": "Europe/Moscow",
+            }
+            DokerEnv.write_env(DokerEnv.env)
+
+    @staticmethod
+    def write_env(d):
+        with open("env.json", "w") as f:
+            f.write(json.dumps(d))
+            DokerEnv.env = d
+
+    @staticmethod
+    def env_changed(d):
+        changed = json.dumps(d, sort_keys=True) != json.dumps(DokerEnv.env, sort_keys=True)
+        if changed:
+            DokerEnv.write_env(d)
+        return changed
+
+
+DokerEnv.read_env()
 
 
 def set_proces_info(text):
@@ -47,7 +83,8 @@ def check_container_is_running():
         c = matching_containers[0]
         dpg.set_value('container_started', c.name)
         dpg.set_value('combo_version', c.attrs['Config']['Image'])
-        dpg.set_value('input_host', c.attrs['Config']['Env'][0].split("=")[-1])
+        dpg.set_value('combo_timezone', DokerEnv.env["TZ"])
+        dpg.set_value('input_host', DokerEnv.env["HOST"])
         dpg.enable_item('button_stop')
 
     return matching_containers
@@ -55,23 +92,27 @@ def check_container_is_running():
 
 def start_container_by_image_name(image_name, button):
     # Получаем список всех контейнеров
-    containers = client.containers.list(all=True)
-
-    # Проверяем наличие контейнера для выбранного имени образа
-    container_exists = any(container.image.tags[0] == image_name for container in containers)
-
-    # Выводим результат
-    if container_exists:
-        set_proces_info(f"Container {image_name} found")
-    else:
-        set_proces_info(f"Container {image_name} not found")
-
     container_name = get_container_name_by_image(image_name)
+    environment = {
+        'HOST': dpg.get_value('input_host'),
+        'TZ': dpg.get_value('combo_timezone'),
+    }
+    try:
+        container = client.containers.get(container_name)
+        if DokerEnv.env_changed(environment):
+            container.remove()
+            DokerEnv.write_env(environment)
+            container = None
+            print('remove!')
+    except docker.errors.NotFound:
+        container = None
 
-    if not container_exists:
+    if not container:
+        set_proces_info(f"Container {image_name} not found")
         try:
             # Создаем контейнер на основе образа
-            client.containers.create(
+            print('Создаем', DokerEnv.env)
+            container = client.containers.create(
                 image_name,
                 name=container_name,
                 # контейнер_порт / tcp: хост_порт
@@ -82,21 +123,15 @@ def start_container_by_image_name(image_name, button):
                     '1883/tcp': 1883,
                     '8080/tcp': 8888,
                 },
-                environment={
-                    'HOST': dpg.get_value('input_host'),
-                    'TZ': dpg.get_value('combo_timezone'),
-                }
+                environment=DokerEnv.env
             )
+
             set_proces_info(f"Container {container_name} created")
 
         except APIError as e:
             set_proces_info(f"Error create container: {e}")
             return
 
-    if check_container_is_running():
-        return
-
-    container = client.containers.get(container_name)
     if container.status == "running":
         return
 
@@ -116,6 +151,7 @@ def start_container(sender, app_data, user_data):
         return
     if check_container_is_running():
         return
+
     start_container_by_image_name(image_name, button=sender)
 
 
@@ -165,10 +201,13 @@ with dpg.window(label="", width=600, height=600):
                   label="Choose version", width=200,
                   tag='combo_version',
                   default_value=docker_images[0] if docker_images[0] else '')
-    dpg.add_input_text(label="HOST IP ADDRESS", width=200, tag='input_host')
+    dpg.add_input_text(label="HOST IP ADDRESS",
+                       width=200,
+                       tag='input_host',
+                       default_value=DokerEnv.env["HOST"])
     dpg.add_combo(['Asia/Riyadh', 'Asia/Dubai', 'Europe/Moscow'],
                   callback=on_select_images,
-                  label="Choose version", width=200,
+                  label="Choose TimeZone", width=200,
                   tag='combo_timezone',
                   default_value='Europe/Moscow')
     dpg.add_button(label="Start", callback=start_container, tag='button_start', )
