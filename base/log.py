87458c8fb276aafa3fcaf7c42886ed9e02dd46cc
kot6egemot <ex-2k@yandex.ru>
2023-10-12 11:16:42 +0300

    fix logger

--- base/log.py
+++ base/log.py
@@ -1,11 +1,13 @@
 import logging
 from config import BASE_DIR
+from logging.handlers import RotatingFileHandler
+
 logger = logging.getLogger('app')
 logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-file_handler = logging.FileHandler(f'{BASE_DIR}/logs/logfile.log')
+file_handler = RotatingFileHandler(f'{BASE_DIR}/logs/logfile.log', 'a', 1 * 1024 * 1024, 10)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
