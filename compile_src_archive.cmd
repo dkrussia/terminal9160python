dbb79e8c5bd28324ef9de8378d86fa83fb26082c
kot6egemot <ex-2k@yandex.ru>
2023-10-30 16:27:24 +0300

    Добавил скрипты

--- compile_src_archive.cmd
+++ compile_src_archive.cmd
@@ -1,9 +1,16 @@
 :: Запуск ./compile_src_archive.cmd env_mode
 set env_mode=%1
 echo "Dashboard build %env_mode%"
+
 cd frontend
 call npm run build -- --mode ".env.%env_mode%"
 cd ..
+
+call ./archive_whl_packages.cmd
+
 echo "Compile an archive..."
-tar -acf 9160-service-%env_mode%.zip base dashboard services utils config.py main.py requirements.txt
-echo "Finished"
+tar -acf 9160-service-%env_mode%.zip base dashboard services utils config.py main.py requirements.txt run.cmd install_requirements_offline.cmd packages.zip
+
+del .\packages.zip
+
+echo "Finished build src"
