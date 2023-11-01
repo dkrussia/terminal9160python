d2bda9c9eab82a801e529d1ffa760cfc5ea05c7f
kot6egemot <ex-2k@yandex.ru>
2023-11-01 13:12:51 +0300

    fix

--- docker_build_push.cmd
+++ docker_build_push.cmd
@@ -1,5 +1,5 @@
 cd frontend
-call npm run build -- --mode .env.msk-dormakaba
+call npm run build -- --mode msk-dormakaba
 cd ..
 docker build . -t terminal9160-async
 docker tag terminal9160-async kuznetsovsergey/9160:v1
