33071a0800f1de9caf014b59b4d1ec705bcbaa0c
kot6egemot <ex-2k@yandex.ru>
2024-04-02 18:02:55 +0300

    fix sync

--- sync.py
+++ sync.py
@@ -5,6 +5,7 @@ from asyncio import set_event_loop_policy, WindowsSelectorEventLoopPolicy
 
 from base.bookings.sync import sync_booking_all_devices
 from base.mqtt_client import mqtt_consumer
+from base.rmq_client import rabbit_mq
 from services.devices_storage import device_service
 
 set_event_loop_policy(WindowsSelectorEventLoopPolicy())
@@ -12,12 +13,12 @@ set_event_loop_policy(WindowsSelectorEventLoopPolicy())
 devices = device_service.all_sn_list
 date = datetime.datetime.now() - datetime.timedelta(days=1)
 
-print(date)
-print(devices)
-
-
 async def main():
+    def reconnect():
+        pass
+
     asyncio.create_task(mqtt_consumer())
+    await rabbit_mq.start(reconnect)
     await sync_booking_all_devices(devices, date, from_matrix=True)
 
 
